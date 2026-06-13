bbb98298098dfa7c621631535a2b43a7422101c8
aakhan917720 <asamadk112452@gmail.com>
2026-06-13 05:21:51 +0500

    Forgot button is added

--- lib/firebase_serivces/splash_serivces.dart
+++ lib/firebase_serivces/splash_serivces.dart
@@ -24,7 +24,7 @@ void login(BuildContext context){
     Timer(Duration(seconds: 2), ()=>Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context)=>UploadImage(),
+        builder: (context)=>PostScreen(),
       ),
     )
     );
